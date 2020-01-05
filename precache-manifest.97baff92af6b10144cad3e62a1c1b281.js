139a5cf616a3e1624872f9d5ef1c6560c54921ce
tisanthan <tisanthan12@gmail.com>
2020-01-05 15:29:11 +0530

    Updates

---
+++ precache-manifest.97baff92af6b10144cad3e62a1c1b281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e630911290fe34eb2135fd78f399a9b",
+    "url": "/few/index.html"
+  },
+  {
+    "revision": "30bf841acab4985ba223",
+    "url": "/few/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "95ce7c7fcce0a7a9586d",
+    "url": "/few/static/js/2.fcbbc4e1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/few/static/js/2.fcbbc4e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30bf841acab4985ba223",
+    "url": "/few/static/js/main.cce44436.chunk.js"
+  },
+  {
+    "revision": "917a2ca804e6ff337525",
+    "url": "/few/static/js/runtime-main.8131b364.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/few/static/media/logo.5d5d9eef.svg"
+  }
+]);
